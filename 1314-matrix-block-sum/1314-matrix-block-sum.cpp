4316aaa3cadf6dc753fa976ebb9b4b43dfdaa261
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-02-18 01:22:36 +0530

    Time: 21 ms (38.84%), Space: 9.5 MB (55.18%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -3,21 +3,40 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int n=mat.size();
         int m =mat[0].size();
+        vector<vector<int>>dp(n,vector<int>(m));
+        for(int i=0;i<n;i++)
+        {
+            int sum =0;
+            for(int j=0;j<m;j++)
+            {
+                sum+= mat[i][j];
+                dp[i][j]=sum;
+            }
+        }
+        
         vector<vector<int>> ans(n,vector<int> (m));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                for(int r=i-k;r<=i+k;r++)
+                int rstart = max(i-k,0);
+                int rend = min(i+k,n-1);
+                int colstart = max(j-k,0);
+                int colend = min(j+k,m-1);
+
+                int a = colstart-1;
+                int sum =0;
+                if(a<0)
                 {
-                    for(int c=j-k;c<=j+k;c++)
-                    {
-                        if(r>=0 && r<n && c>=0 && c<m)
-                        {
-                            ans[i][j]+=mat[r][c];
-                        }
-                    }
+                    for(int r=rstart;r<=rend;r++)
+                        sum+= dp[r][colend];
                 }
+                else
+                {
+                    for(int r=rstart;r<=rend;r++)
+                        sum+= (dp[r][colend]-dp[r][a]);
+                }
+                ans[i][j]=sum;
             }
         }
         return ans; 
