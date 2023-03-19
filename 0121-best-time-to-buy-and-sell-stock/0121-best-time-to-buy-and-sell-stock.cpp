b4ffb5b4c418ced5018931ad1ef0e500f1a82666
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-03-20 00:49:04 +0530

    Time: 141 ms (49.47%), Space: 97.2 MB (5.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& p) {
-        vector<int> ivec(p.size(),-1);
-        for(int i=0;i<p.size();i++)
-            ivec[i] = -p[i];
-        
-        vector<int> maxvec(p.size(),p[p.size()-1]);
-        
-        for(int i=p.size()-2;i>=0;i--)
-            maxvec[i]= max(maxvec[i+1],p[i]);
-        
-        int ans = INT_MIN;
-        for(int i=0;i<p.size()-1;i++)
+    int maxProfit(vector<int>& prices) {
+        vector<int> temp(prices.size(),prices[prices.size()-1]);
+        for(int i=prices.size()-2;i>=0;i--)
         {
-            int temp = ivec[i]+maxvec[i+1];
-            ans = max(ans,temp);
+            temp[i]=max(temp[i+1],prices[i]);
         }
-        if(ans<0)   return 0;
+        int ans=0;
+        for(int i=0;i<prices.size()-1;i++)
+        {
+            ans= max(ans,temp[i+1]-prices[i]);
+        }
         return ans;
     }
 };
