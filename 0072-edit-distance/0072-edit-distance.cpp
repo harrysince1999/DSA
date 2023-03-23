97e80c254d4d51065976090efc9a85b580a4f268
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-03-23 12:06:40 +0530

    Time: 89 ms (9.83%), Space: 83.6 MB (5.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int solve(string a, string b, int i, int j,vector<vector<int>>& dp)
-    {
-        if(i<0)    return j+1;
-        if(j<0)     return i+1;
+    int solve(string a, int i, string b, int j,vector<vector<int>>&dp){
+        
+        if(i>a.size())  return b.size()-j+1;
+        if(j>b.size())  return a.size()-i+1;
+        
         if(dp[i][j]!=-1)    return dp[i][j];
+        if(a[i]==b[j])
+            return dp[i][j]=solve(a,i+1,b,j+1,dp);
         
-        if(a[i]==b[j])  return dp[i][j]=solve(a,b,i-1,j-1,dp);
+        int x = 1+solve(a,i+1,b,j,dp);
+        int y = 1+solve(a,i+1,b,j+1,dp);
+        int z = 1+solve(a,i,b,j+1,dp);
         
-        else
-        {
-            int x = 1+solve(a,b,i-1,j,dp);
-            int y = 1+solve(a,b,i,j-1,dp);
-            int z = 1+solve(a,b,i-1,j-1,dp);
-            return dp[i][j]=min(x,min(y,z));
-        }
+        return dp[i][j]=min(x,min(y,z));
     }
     int minDistance(string a, string b) {
-        vector<vector<int>> dp(a.size()+1,vector<int>(b.size()+1,-1));
-        return solve(a,b,a.size(),b.size(),dp);
+        vector<vector<int>>dp(a.size()+1,vector<int>(b.size()+1,-1));
+        return solve(a,0,b,0,dp);
     }
 };
