042b8e8002fac4c7cf6e71d996f2a8c6326b549a
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-04-04 10:59:54 +0530

    Time: 269 ms (17.75%), Space: 72.1 MB (8.67%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int ans = 0;
+        int ans = 1;
         set<int>st;
         for(int i=0;i<s.size();i++)
         {
-            if(st.find(s[i])==st.end())     // present nhi hai
-            {
-                st.insert(s[i]);
-            }
-            else                // present hai
+            if(st.find(s[i])!=st.end()) 
             {
                 ans++;
                 st.clear();
-                st.insert(s[i]);
             }
+            st.insert(s[i]);
         }
-        if(!st.empty()) ans++;
         return ans;
     }
 };
