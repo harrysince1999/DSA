debce4ea7c2737711360fe89959f374ba63bb893
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-04-06 20:07:23 +0530

    Time: 24 ms (46.13%), Space: 26.5 MB (23.78%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>&inorder, int is, int ie,
-                   vector<int>&postorder, int ps, int pe, map<int,int>& mp)
+    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int is, int ie,
+                   int ps, int pe, map<int,int>& mp)
     {
         if(is>ie || ps>pe)  return NULL;
+        
         TreeNode* root = new TreeNode(postorder[pe]);
-        int idx = mp[postorder[pe]];
+        int idx = mp[root->val];
         int rem = idx-is;
         
-        root->left = solve(inorder,is,idx-1,postorder,ps,ps+rem-1,mp);
-        root->right = solve(inorder,idx+1,ie,postorder,ps+rem,pe-1,mp);
+        root->left = solve(inorder,postorder,is,idx-1,ps,ps+rem-1,mp);
+        root->right = solve(inorder,postorder,idx+1,ie,ps+rem,pe-1,mp);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int,int>mp;
+        map<int,int> mp;
         for(int i=0;i<inorder.size();i++)
             mp[inorder[i]]=i;
-        
-        return solve(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,mp);
+        TreeNode* root = solve(inorder,postorder, 0, inorder.size()-1, 
+                               0, postorder.size()-1, mp);
+        return root;
     }
 };
