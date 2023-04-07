70d798058fdd661d64417529f2181aa6cb1a765d
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-04-07 21:49:26 +0530

    Time: 82 ms (47.19%), Space: 29.2 MB (65.04%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    bool isPossible(vector<vector<int>>& grid, int i, int j, int r, int c)
+    void dfs(vector<vector<int>>& grid, int i, int j)
     {
-        if(i<r && i>=0 && j<c && j>=0 && grid[i][j]==1)
-            return true;
-        return false;
-    }
-    void dfs(vector<vector<int>>& grid, int i, int j, int r, int c)
-    {
-        grid[i][j]=0;
+        if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 || grid[i][j]!=1) return;
         
-        if(isPossible(grid,i+1,j,r,c))
-            dfs(grid,i+1,j,r,c);
-        if(isPossible(grid,i,j+1,r,c))
-            dfs(grid,i,j+1,r,c);
-        if(isPossible(grid,i-1,j,r,c))
-            dfs(grid,i-1,j,r,c);
-        if(isPossible(grid,i,j-1,r,c))
-            dfs(grid,i,j-1,r,c);
+        grid[i][j]=0;
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        int r= grid.size();
-        int c= grid[0].size();
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(i*j==0 || i==r-1 || j==c-1){
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[0].size();j++)
+                if(i*j==0 || i==grid.size()-1 || j==grid[0].size()-1)
                     if(grid[i][j]==1)
-                        dfs(grid,i,j,r,c);
-                }
-            }
-        }
+                        dfs(grid,i,j);
+
         int ans = 0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j]==1)
-                    ans++;
-            }
-        }
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[0].size();j++)
+                if(grid[i][j]==1)   ans++;
+        
         return ans;
     }
 };
