25c45db5435b15901c1f58d017907854acfc1004
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-04-19 17:01:12 +0530

    Time: 22 ms (8.92%), Space: 15.8 MB (85.60%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,35 +2,41 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
-        int e1=-1;
-        int e2=-1;
-        int c1=0;
-        int c2=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==e1) c1++;
-            else if(nums[i]==e2)    c2++;
-            else if(c1==0){
-                e1=nums[i];
+        int e1 = -1, e2= -1, c1=0, c2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==e1)
                 c1++;
-            }
-            else if(c2==0){
-                e2=nums[i];
+            else if(nums[i]==e2)
                 c2++;
+            else if(c1==0)
+            {
+                e1 = nums[i];
+                c1 = 1;
             }
-            else{
+            else if(c2==0)
+            {
+                e2 = nums[i];
+                c2 = 1;
+            }
+            else
+            {
                 c1--;
                 c2--;
             }
         }
-        c1=0,c2=0;
-        for(int i=0;i<nums.size();i++)
+        
+        c1 = 0, c2 = 0;
+        for(int i=0; i<nums.size();i++)
         {
-            if(nums[i]==e1) c1++;
-            else if(nums[i]==e2) c2++;
+            if(nums[i]==e1)
+                c1++;
+            else if(nums[i]==e2)
+                c2++;
         }
-        if(c1>nums.size()/3)    ans.push_back(e1);
-        if(c2>nums.size()/3)    ans.push_back(e2);
+        if(c1>nums.size()/3)
+            ans.push_back(e1);
+        if(c2>nums.size()/3)
+            ans.push_back(e2);
         return ans;
     }
 };
