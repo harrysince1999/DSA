5b752b80e7f4e084f121823e22f09c0444de1544
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2023-05-01 08:29:19 +0530

    Time: 5 ms (48.10%), Space: 11.1 MB (80.50%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int find(vector<int>& nums)
+    {
+        int l = 0;
+        int h = nums.size()-1;
         int n = nums.size();
-        int i=0, j= n-1;
+        while(l<=h)
+        {
+            int mid = (l+h)/2;
+            int prev = (mid-1+n)%n;
+            int next = (mid+1)%n;
+            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next])
+                return mid;
+            else if(nums[mid]<=nums[n-1])
+                h= mid-1;
+            else if(nums[mid]>=nums[0])
+                l = mid+1;
+        }
+        return 0;
+    }
+    int search(vector<int>& nums, int target) {
+        int minIdx = find(nums);
+        //cout<<minIdx<<" ";
+        int n = nums.size();
+        int i,j;
+        if(target<=nums[n-1] && target>=nums[minIdx])
+        {
+            i=minIdx;
+            j=n-1;
+        }
+        else
+        {
+            i=0;
+            j=minIdx-1;
+        }
+        
         while(i<=j)
         {
-            int mid = (j-i)/2+i;
+            int mid = (i+j)/2;
             if(nums[mid]==target)   return mid;
-            else if(nums[mid]>=nums[i]) // left side strictly increasing hai
-            {
-                if(target<=nums[mid] && target>=nums[i])
-                    j= mid-1;
-                else
-                    i=mid+1;
-            }
+            else if(nums[mid]<target)
+                i= mid+1;
+
             else
-            {
-                if(target<=nums[j] && target>=nums[mid])
-                    i=mid+1;
-                else
-                    j=mid-1;
-            }
+                j=mid-1;
         }
         return -1;
     }
